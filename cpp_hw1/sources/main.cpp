9fe3aa125cce00dcee913c890689f811fddd916e
frosnikny <frosnikny@gmail.com>
2023-03-19 11:31:16 +0300

    Add handle_args function

--- cpp_hw1/sources/main.cpp
+++ cpp_hw1/sources/main.cpp
@@ -8,12 +8,46 @@
 
 std::map<std::string, std::string> take_args(int argc, char* argv[]);
 
+std::map<std::string, std::string> handle_args(int argc, char* argv[],
+                                               int& max_duration);
+
 int main(int argc, char* argv[]) {
+  int required_best_numbers = 10;
+  int max_duration = 0;
+  std::map<std::string, std::string> args_filenames =
+      handle_args(argc, argv, max_duration);
+
+  try {
+    runTask(std::cout, args_filenames["--title-basics-path"],
+            args_filenames["--title-episodes-path"],
+            args_filenames["--title-ratings-path"],
+            args_filenames["--title-akas-path"], max_duration,
+            required_best_numbers);
+  } catch (std::exception&(e)) {
+    std::cerr << "An error occurred while the program was running\n"
+              << e.what() << "\n";
+    exit(1);
+  }
+  return 0;
+}
+
+std::map<std::string, std::string> take_args(int argc, char* argv[]) {
+  std::map<std::string, std::string> arguments{};
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+    if (arg.substr(0, 2) == "--") {
+      arguments[arg] = argv[++i];
+    }
+  }
+  return std::move(arguments);
+}
+
+std::map<std::string, std::string> handle_args(int argc, char* argv[],
+                                               int& max_duration) {
   if (argc != 11) {
     reportInvalidArgumentError(
         std::invalid_argument("Invalid number of arguments received"), argv[0]);
   }
-  int max_duration{};
 
   std::map<std::string, std::string> args_filenames = take_args(argc, argv);
   if (args_filenames.find("--title-basics-path") == args_filenames.end() ||
@@ -47,29 +81,6 @@ int main(int argc, char* argv[]) {
     std::cerr << "The --max_duration argument should be greater than 0\n";
     exit(1);
   }
-  int required_best_numbers = 10;
 
-  try {
-    runTask(std::cout, args_filenames["--title-basics-path"],
-            args_filenames["--title-episodes-path"],
-            args_filenames["--title-ratings-path"],
-            args_filenames["--title-akas-path"], max_duration,
-            required_best_numbers);
-  } catch (std::exception&(e)) {
-    std::cerr << "An error occurred while the program was running\n"
-              << e.what() << "\n";
-    exit(1);
-  }
-  return 0;
-}
-
-std::map<std::string, std::string> take_args(int argc, char* argv[]) {
-  std::map<std::string, std::string> arguments{};
-  for (int i = 1; i < argc; ++i) {
-    std::string arg = argv[i];
-    if (arg.substr(0, 2) == "--") {
-      arguments[arg] = argv[++i];
-    }
-  }
-  return std::move(arguments);
+  return std::move(args_filenames);
 }
